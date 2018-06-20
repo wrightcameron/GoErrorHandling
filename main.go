08a8504672c31b40546cd1d7f9023d1595614731
Cameron Wright <cameronwright@u.boisestate.edu>
2018-06-19 20:47:18 -0600

    Build a new project focus
    The new project focus is on a program where when an address is given
    a sunrise and sunset is given,  this invoves doing a get request to
    google maps to get the gps coordinates from the address and using
    those coordinates to find what the sun rise and sunset is with a
    library that does all the astronomy math calculations.

--- main.go
+++ main.go
@@ -3,48 +3,93 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io/ioutil"
-	"strings"
+	"os"
+	"regexp"
+	"strconv"
+	"time"
 
 	"github.com/fatih/color"
+	"github.com/nathan-osman/go-sunrise"
+	"gopkg.in/resty.v1"
 )
 
 func main() {
-	data := readTxtFile("Gettysburg-Address.txt")
-	findUpperCaseWords(data)
-}
-
-func findUpperCaseWords(text string) {
-	//First Scanner breaks text into lines
-	lineScanner := bufio.NewScanner(strings.NewReader(text))
-	lineScanner.Split(bufio.ScanLines)
-	for lineScanner.Scan() {
-		wordScanner := bufio.NewScanner(strings.NewReader(lineScanner.Text()))
-		wordScanner.Split(bufio.ScanWords)
-		for wordScanner.Scan() {
-			word := wordScanner.Text()
-			if isUpperCase(word) {
-				color.Red(word)
-			} else {
-				color.Blue(word)
-			}
-		}
-		//fmt.Println()
-	}
-}
-
-func isUpperCase(word string) bool {
-	if word[0] >= 65 && word[0] <= 90 {
-		return true
-	}
-	return false
-}
-
-func readTxtFile(fileName string) string {
-	data, err := ioutil.ReadFile(fileName)
+	//TODO User inputs address
+	fmt.Println("Find out when the Sun will rise and set at your location.")
+	fmt.Println("Input your address")
+	reader := bufio.NewReader(os.Stdin)
+	address, _ := reader.ReadString('\n')
+	//TODO Get request to google
+	resp, err := resty.R().
+		SetQueryParams(map[string]string{
+			"address": address,
+		}).
+		SetHeader("Accept", "application/json").
+		Get("http://maps.google.com/maps/api/geocode/json")
 	if err != nil {
-		fmt.Println("Can't read file, or does not exist", fileName)
-		panic(err)
+		fmt.Printf("\nError: %v", err)
 	}
-	return string(data)
+	//fmt.Printf("\nResponse Body: %v", resp)
+	//fmt.Println(reflect.TypeOf(resp.String()))
+
+	//TODO Parse JSON for lat, lng
+	pat := regexp.MustCompile(`"location"\s*:\s*{\s*"lat"\s*:\s-?\d+.\d+,\s*"lng"\s*:\s*-?\d+.\d+\s*}`)
+	s := pat.FindString(resp.String())
+	//fmt.Println(s)
+
+	pat = regexp.MustCompile(`-?\d+.\d+`)
+	coordinates := pat.FindAllString(s, -1)
+	//fmt.Println(coordinates)
+	latitude, _ := strconv.ParseFloat(coordinates[0], 64)
+	longitude, _ := strconv.ParseFloat(coordinates[1], 64)
+	//fmt.Println(latitude)
+	//fmt.Println(longitude)
+	//TODO put lat, lng into sunrise sunset
+	rise, set := sunrise.SunriseSunset(longitude, latitude, 2018, time.June, 19)
+	//TODO display the sunrise and sunset
+	color.Blue("For the address of :" + address)
+	color.Blue("Sunrise is at " + rise.String())
+	color.Blue("Sunset is at " + set.String())
+	//getGoogleMapsCoord()
+	// rise, set := sunrise.SunriseSunset(
+	// 	43.607561, -116.205834, // Downtown Boise
+	// 	2018, time.June, 19, // 2000-01-01
+	// )
+	// fmt.Println(rise)
+	// fmt.Println(set)
+	// calSunRiseSunSet(43.607561, -116.205834, 2018, time.June, 19)
+	//data := readTxtFile("Gettysburg-Address.txt")
+	//findUpperCaseWords(data)
+}
+
+func getCoordFromAddress(address string) {
+
+}
+
+func getGoogleMapsCoord() {
+	//resp, err := resty.R().Get("http://maps.google.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA")
+
+	resp, err := resty.R().
+		SetQueryParams(map[string]string{
+			"address": "12851 West Silverbrook Ct Boise ID",
+		}).
+		SetHeader("Accept", "application/json").
+		Get("http://maps.google.com/maps/api/geocode/json")
+
+	// explore response object
+	fmt.Printf("\nError: %v", err)
+	fmt.Printf("\nResponse Status Code: %v", resp.StatusCode())
+	fmt.Printf("\nResponse Status: %v", resp.Status())
+	fmt.Printf("\nResponse Time: %v", resp.Time())
+	fmt.Printf("\nResponse Received At: %v", resp.ReceivedAt())
+	fmt.Printf("\nResponse Body: %v", resp) // or resp.String() or string(resp.Body())
+
+}
+
+func calSunRiseSunSet(long float64, lat float64, year int, month time.Month, day int) (time.Time, time.Time) {
+	rise, set := sunrise.SunriseSunset(
+		long, lat,
+		year, month, day,
+	)
+	return rise, set
 }
